0555cb961be363cea1f36a3b4aac37e5b6495260
katerynabugaieva <sonnensmile@gmail.com>
2019-10-11 10:02:55 +0200

    login and validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+form: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
   }
 
+  onSubmit(){
+    console.log(this.form)
+  }
 }
