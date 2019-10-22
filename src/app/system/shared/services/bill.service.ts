c8df2177e5e9c301fd81c7657606b73dd0dbc6c4
katerynabugaieva <sonnensmile@gmail.com>
2019-10-22 17:27:44 +0200

    :poop: shared module

--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Bill } from '../models/bill.model';
+import { BaseApi } from 'src/app/shared/core/base-api';
 
 @Injectable()
-export class BillService {
-    constructor(private http: Http) {
-
+export class BillService extends BaseApi {
+    constructor(public http: Http) {
+        super(http)
     }
-    getBill(): Observable<any> {
-        return this.http.get('http://localhost:3000/')
-            .map((response: Response) => response.text())
+    /*
+    getBill(): Observable<Bill> {
+        return this.get('bill')
+        //return this.http.get('http://localhost:3000/')
+        //    .map((response: Response) => response.text())
         //  .map((response: Response) => response.json());
+    }
+    */
+    getBill(): Observable<Bill> {
+        return this.get('bill');
     }
 
     getCurrency(base: string = 'UAH'): Observable<any> {
