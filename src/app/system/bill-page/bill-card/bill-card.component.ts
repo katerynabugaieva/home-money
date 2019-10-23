57540e827158ed4ad8fcb80cfe9e37f769129f3e
katerynabugaieva <sonnensmile@gmail.com>
2019-10-23 11:01:21 +0200

    :heavy_plus_sign: fullfiling page with logic

--- src/app/system/bill-page/bill-card/bill-card.component.ts
+++ src/app/system/bill-page/bill-card/bill-card.component.ts
@@ -8,11 +8,14 @@ import { Bill } from '../../shared/models/bill.model';
 })
 export class BillCardComponent implements OnInit {
   @Input() bill: Bill;
-  @Input() currency: any
+  @Input() currency: any;
+  dollar: number;
   constructor() { }
 
   ngOnInit() {
-    console.log(this.currency)
+    const { rates } = this.currency;
+    this.dollar = rates['USD'] * this.bill.value
+    
   }
 
 }
--- src/app/system/bill-page/currency-card/currency-card.component.ts
+++ src/app/system/bill-page/currency-card/currency-card.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-currency-card',
   templateUrl: './currency-card.component.html',
   styleUrls: ['./currency-card.component.css']
 })
-export class CurrencyCardComponent implements OnInit {
+export class CurrencyCardComponent {
+  @Input() currency: any;
+  currencies: string[] = ["USD", "EUR"]
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 }
