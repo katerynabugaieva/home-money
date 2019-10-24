8aba2b8a735d9a348b74ccb4ae4a8d0d5204ee78
katerynabugaieva <sonnensmile@gmail.com>
2019-10-24 10:51:50 +0200

    :busts_in_silhouette: adding components logic

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CategoriesService } from '../../shared/services/categories.service';
+import { Category } from '../../shared/models/category.model';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent {
+  @Output() onCategoryAdd = new EventEmitter<Category>();
+  constructor(private categoriesService: CategoriesService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(form: NgForm) {
+    let { name, capacity } = form.value;
+    if (capacity < 0) capacity *= -1;
+    const category = new Category(name, capacity)
+    this.categoriesService.addCategory(category)
+      .subscribe((category: Category) => {
+        form.reset();
+        form.form.patchValue({ capacity: 1 })
+        this.onCategoryAdd.emit(category)
+      });
   }
-
 }
---
+++ src/app/system/shared/services/categories.service.ts
@@ -0,0 +1,16 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { BaseApi } from 'src/app/shared/core/base-api';
+import { Category } from '../models/category.model';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CategoriesService extends BaseApi {
+    constructor(public http: Http) {
+        super(http)
+    }
+
+    addCategory(category: Category): Observable<Category> {
+        return this.post('categories', category)
+    }
+}
