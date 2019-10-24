16614561555c59f533c36a48fc3b2af366f683e5
katerynabugaieva <sonnensmile@gmail.com>
2019-10-24 14:16:19 +0200

    :boom: update logic

--- src/app/system/records-page/records-page.component.ts
+++ src/app/system/records-page/records-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../shared/models/category.model';
+import { CategoriesService } from '../shared/services/categories.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-records-page',
@@ -7,13 +9,24 @@ import { Category } from '../shared/models/category.model';
   styleUrls: ['./records-page.component.css']
 })
 export class RecordsPageComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[] = [];
+  isLoaded: boolean = false;
+  constructor(private categoriesService: CategoriesService) { }
 
   ngOnInit() {
+    this.categoriesService.getCategories()
+      .subscribe((categories: Category[]) => {
+        this.categories = categories;
+        this.isLoaded = true
+      })
   }
   newCategoryAdded(category: Category) {
-    //to do: add
+    this.categories.push(category)
+  }
+  categoryWasEdit(category: Category) {
+    const idx = this.categories
+      .findIndex(c => c.id === category.id);
+    this.categories[idx] = category;
   }
 
 }
