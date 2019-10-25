81bc423483cb86c37469f399cd29cd38fd340eec
katerynabugaieva <sonnensmile@gmail.com>
2019-10-25 21:46:46 +0200

    add event logic

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
+import { NgForm } from '@angular/forms';
+import { AppEvent } from '../../shared/models/event.model';
+import * as moment from 'moment'
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.css']
 })
 export class AddEventComponent implements OnInit {
-
+  @Input() categories: Category[] = []
+  types = [
+    { type: 'income', label: 'Income' },
+    { type: 'outcome', label: 'Outcome' }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    let { amout, description, category, type } = form.value;
+    if (amout < 0) amout *= -1;
+
+    const event = new AppEvent(type,
+      amout,
+      +category,
+      moment().format('dd.MM.yyyy')
+      , description)
+  }
 }
